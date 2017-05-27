507fd3f3d356e6d1cbd419ee0517d9c3b8bc6f33
renato ribeiro <ribeiro170@gmail.com>
2017-05-27 15:32:26 -0300

    acerto nos metodos de teste pagamento dinheiro

--- app/src/test/java/ipobre/ipobre/PagamentoDineheiroTest.java
+++ app/src/test/java/ipobre/ipobre/PagamentoDineheiroTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import ipobre.ipobre.model.MeioPagamento;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
 /**
@@ -11,17 +13,37 @@ import static org.junit.Assert.assertEquals;
  */
 public class PagamentoDineheiroTest {
 
-    private MeioPagamento meioPagamento = new MeioPagamento(true,0.1);
+    private MeioPagamento meioPagamento = new MeioPagamento();
 
     //VERIFICA SE O PAGAMENTO É EM DINHEIRO
     @Test
-    public void ehDinheiro()throws Exception {
-        assertEquals(true,meioPagamento.isPagamentoDinheiro());
+    public void ehDinheiro(){
+        this.preencheMeioPagemtnoTeste();
+        assertTrue("não é pagamento em dinheiro",meioPagamento.isPagamentoDinheiro());
+
     }
 
     @Test
     public void valorPreenchidoValido()throws Exception {
-        assertEquals(true,meioPagamento.valorDinheiroPreenchido());
+        this.preencheMeioPagemtnoTeste();
+        assertTrue("ValorPreenchido Invalido",meioPagamento.valorDinheiroPreenchido());
+    }
+
+    @Test
+    public void NaoehDinheiro()throws Exception {
+        this.preencheMeioPagemtnoTeste();
+        assertFalse("é Dinheiro",meioPagamento.isPagamentoDinheiro());
+    }
+
+    @Test
+    public void valorPreenchidoInvalido()throws Exception {
+        this.preencheMeioPagemtnoTeste();
+        assertFalse("Valor Preenchido valido",meioPagamento.valorDinheiroPreenchido());
+    }
+
+    private void preencheMeioPagemtnoTeste(){
+        this.meioPagamento.setPagamentoDinheiro(false);
+        this.meioPagamento.setValorDinheiro(0.1);
     }
 
 
